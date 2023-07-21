4f400cbc203ac7e65ef0ee1a4e178026cdcc509b
jayphur <justin.2018.phr@gmail.com>
2023-07-20 18:35:59 -0700

    Cleaner/better architecture, cleaner generics

--- crates/core/src/obj/dim.rs
+++ crates/core/src/obj/dim.rs
@@ -1,20 +1,27 @@
 use std::{fmt::Debug, marker::PhantomData};
+use prelude::*;
+use super::{pos::GlobalPos, map_gen::MapGen};
 
 /// Dimension
-pub struct Dim<Map, MapGen, Vox> where
-Map: ElemMapTrait<Vox, MapGen> + VoxMapTrait<Vox, MapGen>,
+pub struct Dim<Map, MapGen, Vox, Elem> where
+Map: MapTrait<Vox, Elem>,
 {
     map: Map,
+    map_gen: MapGen,
+
     //markers
     voxel: PhantomData<Vox>,
-    map_gen: MapGen,
+    element: PhantomData<Elem>,
 }
 
-pub trait VoxMapTrait<Vox, Gen>: Default + Debug{
-    type Proxy;
+pub trait MapTrait<V, E>: Default + Debug{
+    type MapProxy: MapProxy<V, E, Self>;
+    type GenResult; //How should the generator return results?
 
+    /// Should generate at least this area. if it doesn't exist.
+    fn ensure_radius<G>(center: GlobalPos, radius: u16, world_gen: &G) -> Result<()> where
+    G: MapGen<V, E, Self>;
 }
-
-pub trait ElemMapTrait<Elem, Gen>: Default + Debug{
-    type Proxy;
+pub trait MapProxy<V, E, M: MapTrait<V,E>>{
+    
 }
--- crates/core/src/obj/map_gen.rs
+++ crates/core/src/obj/map_gen.rs
@@ -1,12 +1,13 @@
 use prelude::*;
 
-use super::pos::GlobalPos;
+use super::{pos::GlobalPos, dim::MapTrait};
 
-pub trait MapGenerator<Vox, Elem>: Debug + Default + Clone{
+pub trait MapGen<V, E, M>: Debug + Default + Clone where
+M: MapTrait<V, E>,
+{
     type Seed: Clone;
-    type GenerationResult;
 
     fn set_seed(&mut self, seed: &Self::Seed) -> Result<()>;
     //TODO: something to do with get(GlobalPos) -> ...
-    fn generate_at(&self, pos: GlobalPos) -> Self::GenerationResult;
+    fn generate_at(&self, pos: GlobalPos) -> M::GenResult;
 }
